397e403ab717c586276d1837ea37968e65d72204
EVGENY <evro23x@mail.ru>
2018-05-29 15:18:42 +0300

    add yii2 app

---
+++ cv/common/config/main.php
@@ -0,0 +1,19 @@
+<?php
+return [
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm'   => '@vendor/npm-asset',
+    ],
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'components' => [
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'view' => [
+            'theme' => [
+                'pathMap' => ['@app/views' => '@app/themes/red-zen'],
+                'baseUrl' => '@web/../themes/red-zen',
+            ],
+        ],    
+    ],
+];
---
+++ cv/frontend/views/site/theater.php
@@ -0,0 +1,12 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'theater';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+
+
+</div>
